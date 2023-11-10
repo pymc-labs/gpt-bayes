eda47af7c5dbfa2e4afcf9968a89836086add20e
Thomas Wiecki <thomas.wiecki@gmail.com>
2023-11-10 16:13:21 +0100

    Add debug info.

--- app.py
+++ app.py
@@ -2,14 +2,25 @@ from flask import Flask, request, jsonify
 import pymc as pm
 import numpy as np
 import json
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
 @app.route('/run_dynamic_pymc_model', methods=['POST'])
 def run_dynamic_pymc_model():
+    # Log the raw request data for debugging
+    logging.debug("Received request data: %s", request.data)
+
     # Extract model code and data from the request
     model_code = request.json.get('model_code')
     data_dict = request.json.get('data', {})
+
+    # Log extracted model code and data for debugging
+    logging.debug("Model code: %s", model_code)
+    logging.debug("Data: %s", data_dict)
 
     # Validate the model code and data
     # IMPORTANT: Ensure the model_code and data do not contain malicious elements
@@ -17,18 +28,24 @@ def run_dynamic_pymc_model():
     # Execute the model with data
     results = eval_model_code(model_code, data_dict)
 
+    # Log results for debugging
+    logging.debug("Model results: %s", results)
+
     return jsonify(results)
 
 def eval_model_code(model_code, data_dict):
     # Dynamically execute the model code with data
-    # WARNING: Ensure robust validation of model_code and data to prevent security risks.
     try:
         # Make data_dict available in the local scope for the exec function
         local_scope = data_dict.copy()
         exec(model_code, globals(), local_scope)
-        trace = pm.sample(1000, nuts_sampler="numpyro")
-        return {'summary': str(pm.summary(trace))}
+        trace = pm.sample(1000)
+        summary = pm.summary(trace)
+        # Log summary for debugging
+        logging.debug("Summary: %s", summary)
+        return {'summary': str(summary)}
     except Exception as e:
+        logging.error("Error in eval_model_code: %s", e)
         return {'error': str(e)}
 
 if __name__ == '__main__':
