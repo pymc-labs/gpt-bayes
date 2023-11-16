2242be5502497f470c59eafa477e0e45514a72f0
Thomas Wiecki <thomas.wiecki@gmail.com>
2023-11-16 16:36:02 +0100

    Add debug endpoint.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pymc as pm
+from pymc_marketing.mmm import DelayedSaturatedMMM
 import numpy as np
 import json
 import logging
@@ -16,7 +17,7 @@ def run_dynamic_pymc_model():
 
     # Extract model code and data from the request
     model_code = request.json.get('model_code')
-    data_dict = request.json.get('data_dict', {})
+    data_dict = request.json.get('data_dict')
     # data_dict = {"x": np.array([1., 2., 3.]), "y": np.array([0.5, -1., 2.])}
     # Log extracted model code and data for debugging
     logging.debug("Model code: %s", model_code)
@@ -48,6 +49,65 @@ def eval_model_code(model_code, data_dict):
     except Exception as e:
         logging.error("Error in eval_model_code: %s", e)
         return {'error': str(e)}
+
+@app.route('/debug', methods=['POST'])
+def debug():
+    # Log the raw request data for debugging
+    logging.debug("Received request data: %s", request.data)
+
+    # Extract model code and data from the request
+    a = request.json.get('a')
+    b = request.json.get('b')
+    c = request.json.get('c')
+    logging.debug(f"{a=}\n{b=}\n{c=}")
+
+    return jsonify({"a": a, "b": b, "c": c})
+
+
+@app.route('/run_mmm', methods=['POST'])
+def run_mmm():
+    # Log the raw request data for debugging
+    logging.debug("Received request data: %s", request.data)
+
+    # Extract model code and data from the request
+    model_code = request.json.get('model_code')
+    data_dict = request.json.get('data_dict')
+    # data_dict = {"x": np.array([1., 2., 3.]), "y": np.array([0.5, -1., 2.])}
+    # Log extracted model code and data for debugging
+    logging.debug("Model code: %s", model_code)
+    logging.debug("Data: %s", data_dict)
+
+    # Validate the model code and data
+    # IMPORTANT: Ensure the model_code and data do not contain malicious elements
+
+    # Execute the model with data
+    try:
+        mmm = DelayedSaturatedMMM(
+            date_column="date_week",
+            channel_columns=["x1", "x2"],
+            control_columns=[
+                "event_1",
+                "event_2",
+                "t",
+            ],
+            adstock_max_lag=8,
+            yearly_seasonality=2,
+        )
+        X = data.drop('y',axis=1)
+        y = data['y']
+        mmm.fit(X, y)
+        mmm.plot_components_contributions();
+        
+        logging.debug("Summary: %s", summary)
+        return {'summary': str(summary)}
+    except Exception as e:
+        logging.error("Error in eval_model_code: %s", e)
+        return {'error': str(e)}
+
+    # Log results for debugging
+    logging.debug("Model results: %s", results)
+
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run() #threaded=False, workers=4)
